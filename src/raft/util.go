ea5d17435baa6522a84f09808d4e81b1f9537198
wangzn <wangzn04@gmail.com>
2017-03-13 14:26:44 +0800

    update util

--- src/raft/util.go
+++ src/raft/util.go
@@ -2,9 +2,10 @@ package raft
 
 import "log"
 
-// Debugging
+// Debug defines a flag.
 const Debug = 0
 
+// DPrintf prints debug message.
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 0 {
 		log.Printf(format, a...)
